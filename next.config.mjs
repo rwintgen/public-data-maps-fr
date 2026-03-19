966452ee0ca4bac8e9a83fd09c87b715b887d1c6
Romain Wintgens <rwintgen@student.42madrid.com>
2026-03-19 19:48:28 +0100

    Remove ineffective webpack devtool override

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  webpack: (config, { dev }) => {
-    if (dev) {
-      config.devtool = 'cheap-module-source-map'
-    }
-    return config
-  },
-}
+const nextConfig = {}
 
 export default nextConfig
